297cf8561c757a8c29c08d22035f0b0de2fc3bc1
adetech2017 <adeyemi879@gmail.com>
2022-07-29 12:23:35 +0100

    changes debug mode to true

--- opencall/settings.py
+++ opencall/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&)lm-@bi$54h1as^d-uj27bb6gw!%y&sq_9(jov3(4_rh3=d2e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'lasg-opencall.herokuapp.com']
 
